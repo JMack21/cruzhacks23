b6953979aa0e934b32c4a23ebd7a33c9c113ec28
fjordimm <fjordimm@gmail.com>
2023-02-04 13:53:15 -0800

    did a bunch of stuff fuck you im testing i dont want to describe what i changed

--- silvertrails/app/src/main/java/com/pascalcase/silvertrails/FirstFragment.java
+++ silvertrails/app/src/main/java/com/pascalcase/silvertrails/FirstFragment.java
@@ -46,7 +46,7 @@ public class FirstFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_mapFragment);
             }
         });
 
